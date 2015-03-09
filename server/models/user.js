de8f2ba76885b29ff7ebb9fd1088ffbbda732ba2
emilemuny <emilemuny007@gmail.com>
2015-03-09 16:31:21 -0700

    github Oauth now working.

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,36 @@
 'use strict';
 
-var bcrypt = require('bcrypt');
-var mongoose = require('mongoose');
-var User;
+let bcrypt = require('bcrypt');
+let mongoose = require('mongoose');
+let Request = require('request');
+let qs = require('querystring');
+let User;
 
-var userSchema = mongoose.Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true},
+let userSchema = mongoose.Schema({
+    displayName: String,
+    photoUrl: String,
+    github: String,
     createdAt: {type: Date, default: Date.now, required: true}
 });
+
+userSchema.statics.github = function(payload, cb) {
+  let accessTokenUrl = 'https://github.com/login/oauth/access_token';
+  let userApiUrl = 'https://api.github.com/user';
+  let params = {
+    code: payload.code,
+    client_id: payload.clientId,
+    redirect_uri: payload.redirectUri,
+    client_secret: process.env.GITHUB_SECRET
+
+  };
+  Request.get({url: accessTokenUrl, qs:params}, (err, response, accessToken)=>{
+    let headers = {'User-Agent':'Satellizer'};
+    accessToken = qs.parse(accessToken);
+    //console.log('the access token is ${accessToken}');
+    Request.get({url:userApiUrl, qs:accessToken, headers:headers, json:true}, cb);
+  });
+};
+
 
 userSchema.statics.register = function(o, cb){
   User.findOne({email:o.email}, function(err, user){
@@ -24,7 +46,7 @@ userSchema.statics.authenticate = function(o, cb){
   User.findOne({email:o.email}, function(err, user){
     if (!user) {return cb(true);}
 
-    var isGood = bcrypt.compareSync(o.password, user.password);
+    let isGood = bcrypt.compareSync(o.password, user.password);
     if (!isGood) {return cb(true);}
 
     cb(null, user);
